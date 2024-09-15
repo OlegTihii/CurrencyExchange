5c68103328779e0ccd9d05234127572bd41cb280
OlegTihii <YouDoontKnow>
2024-09-16 00:21:15 +0300

    refactor: extends Exception instead of RuntimeException

--- src/main/java/com/insideprojects/currencyexchange/exception/InvalidInputParametersException.java
+++ src/main/java/com/insideprojects/currencyexchange/exception/InvalidInputParametersException.java
@@ -1,6 +1,6 @@
 package com.insideprojects.currencyexchange.exception;
 
-public class InvalidInputParametersException extends RuntimeException {
+public class InvalidInputParametersException extends Exception {
 
     public InvalidInputParametersException(String message) {
         super(message);
