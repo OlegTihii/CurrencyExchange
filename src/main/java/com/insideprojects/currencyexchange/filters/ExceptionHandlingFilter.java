8ee0b1d8120a3d3a1cdf75afe3fd2b00fd3a76d0
OlegTihii <YouDoontKnow>
2024-09-17 15:00:57 +0300

    refactor: delete frontend

--- src/main/java/com/insideprojects/currencyexchange/filters/ExceptionHandlingFilter.java
+++ src/main/java/com/insideprojects/currencyexchange/filters/ExceptionHandlingFilter.java
@@ -9,11 +9,14 @@ import com.insideprojects.currencyexchange.mapper.ExceptionMapper;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 
 @WebFilter(urlPatterns = "/*")
 public class ExceptionHandlingFilter implements Filter {
+    Logger logger = LogManager.getLogger(ExceptionHandlingFilter.class);
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
@@ -21,11 +24,13 @@ public class ExceptionHandlingFilter implements Filter {
             filterChain.doFilter(request, response);
 
         } catch (InvalidInputParametersException e) {
+            logger.error("", e);
             ((HttpServletResponse) response).setStatus(HttpServletResponse.SC_BAD_REQUEST);
             ExceptionDto exceptionDto = ExceptionMapper.INSTANCE.exceptionToExceptionDto(e);
             response.getWriter().write(new Gson().toJson(exceptionDto));
 
         } catch (CurrencyNotFoundException | CurrencyPairNotFoundException e) {
+            logger.error("", e);
             ((HttpServletResponse) response).setStatus(HttpServletResponse.SC_NOT_FOUND);
             ExceptionDto exceptionDto = ExceptionMapper.INSTANCE.exceptionToExceptionDto(e);
             response.getWriter().write(new Gson().toJson(exceptionDto));
